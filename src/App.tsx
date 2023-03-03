531587ff331a2ee2b753087caec552af7ce42925
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-03 10:12:40 +0400

    ajout stats_globales

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { Component, createSignal, Match, onMount, Show, Switch } from 'solid-js';
+
+// Importations des pages
 import Prediction from './pages/Prediction'
 import Index from './pages/index';
+import Stats_globales from './pages/stats_globales';
+// import Stats_modele from './pages/stats_modele';
 
 import styles from './App.module.css';
 import Navbar from './components/navbar';
 
 
 const App: Component = () => {
-  const page = window.location.pathname.split( '/' )[window.location.pathname.split( '/' ).length - 1]
+
+  // Récuperer le dernier elt de l'url
+  const page = window.location.pathname.split( '/' )[window.location.pathname.split( '/' ).length - 1]; // ATTENTION, implique qu'il n'est pas possible d'avoir des urls dont la fin est identiques 
   
   return (
     <main class={styles.App}>
       <Navbar />
       <Switch fallback={<div>Not Found</div>}>
-        <Match when={page == ""}>
-          <Index />
-        </Match>
-        <Match when={page == "prediction"}>
-          <Prediction />
-        </Match>
+        <Match when={page == ""}> <Index /> </Match>
+        <Match when={page == "prediction"}> <Prediction /> </Match>
+        <Match when={page == "stats_globales"}> <Stats_globales/> </Match>
+        {/* <Match when={page == "stats_modele"}> <Stats_modele/> </Match> */}
       </Switch>
     </main>
   );
