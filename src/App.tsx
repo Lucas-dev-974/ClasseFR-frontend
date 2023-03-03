c78820086027e7051bb1a8808f00b3d262f8c61b
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-03 07:14:58 +0100

    Affichage des notifications

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Index from './pages/index';
 import styles from './App.module.css';
 import Navbar from './components/navbar';
 
+import Notification from './services/notification';
+import { notifs } from './signaux';
 
 const App: Component = () => {
   const page = window.location.pathname.split( '/' )[window.location.pathname.split( '/' ).length - 1]
@@ -12,6 +14,11 @@ const App: Component = () => {
   return (
     <main class={styles.App}>
       <Navbar />
+
+      <Show when={notifs().length > 0}>
+        <Notification notif={notifs()[notifs().length - 1]} />
+      </Show>
+      
       <Switch fallback={<div>Not Found</div>}>
         <Match when={page == ""}>
           <Index />
