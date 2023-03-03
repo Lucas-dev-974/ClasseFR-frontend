3a6d38842e8941127bf673d0ba765ad3861f0660
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-03 11:03:16 +0400

    ajout bouton navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,24 @@
-export default function Navbar(){
+import authenticationCheck from "../utils";
+import { Switch, Match } from "solid-js";
+
+export default function Navbar(page:any){
+    const connexion = authenticationCheck();
+    console.log(page.page)
+    
+    function logout(){
+        localStorage.removeItem('Authorization');
+        window.location.href = '/';
+    }
 
     return(
         <nav class="w-full text-center text-xl flex justify-center items-center" style="height: 64px">
             <a href="/" class="text-center text-white tracking-[.20em]">ClassiFR</a>
-            <div id="loginButton"></div>
+            <Switch>
+                <Match when={connexion} > <button onclick={logout}>se déco</button> </Match>
+                <Match when={!connexion && page.page != "login"}>
+                    <button onClick={ ()=> window.location.href = "/login"}>S'authentifier</button>
+                </Match>
+            </Switch>
         </nav>
     )
 }
