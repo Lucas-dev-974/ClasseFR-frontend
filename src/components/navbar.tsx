2fb1de604a0807b9b63b01c46494415478732f2c
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-03 06:47:20 +0100

    Ajout d'un composant Navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+
+    return(
+        <nav class="w-full text-center text-xl flex justify-center items-center" style="height: 64px">
+            <a href="/" class="text-center text-white tracking-[.20em]">ClassiFR</a>
+            <div id="loginButton"></div>
+        </nav>
+    )
+}
