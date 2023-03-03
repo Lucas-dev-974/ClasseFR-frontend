b04086bd3cd371b622d61da5668b3b912da18853
lucas.lvn <lucas.lvn97439@gmail.com>
2023-03-03 08:08:05 +0100

    code review

--- src/components/SelectModel.tsx
+++ src/components/SelectModel.tsx
@@ -19,7 +19,7 @@ export default function SelectModel(props: any){
       if(typeof(props._onchange) == 'function') props._onchange(e)
     }
 
-    return <select onChange={handleChange} id="models" class="mx-auto w-64 bg-indigo-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    return <select onChange={handleChange} id="models" class="w-64 bg-indigo-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             <option selected>Aucun model sélectionner</option>
             <For each={_models()}>{(model, i) =>
                 <option value={model.id}>{model.name}</option>
