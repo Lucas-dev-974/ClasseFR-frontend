1c6fb712366aa90458286b3fabffea2419300e76
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-06 15:00:51 +0400

    data recup via API

--- src/pages/stats_globales.tsx
+++ src/pages/stats_globales.tsx
@@ -1,56 +1,60 @@
 //Page "Statistiques globales"
 
+// Importations SolidJS
 import {Chart} from "chart.js";
 import { onMount, createResource, createSignal } from "solid-js";
 
+// Importations cutsom functions
 import { request } from "../services/services";
 
 // correction bug pas compris : https://github.com/sgratzl/chartjs-chart-wordcloud/issues/4
 import { registerables } from 'chart.js';
 Chart.register(...registerables);
 //---------------------------------------
-const [totalPredctions, setTotalPredictions] = createSignal()
+
+// Contient le nombre total de prédictions
+const [totalPredctions, setTotalPredictions] = createSignal("") // str vide puis utilisé avec Number()
+
+// Permet la création du pie chart
+const handleGraph = (mauvaisePred:number) => {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+
+    // Création du graph
+    new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Bonnes prédictions', 'Mauvaises prédictions'],
+        datasets: [{
+            label: 'Prédictions',
+            data: [Number(totalPredctions()) - mauvaisePred, mauvaisePred],
+            backgroundColor: [
+                'rgb(54, 162, 235)',
+                'rgb(213,0,5)'],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                labels: {
+                    color: "white"
+                }
+            }
+        }
+    }});
+}
+
+// Requête API
 const stats_globales_request = async () => (await request('api/stats_globales/total_pred', 'GET', null)).json().then(response => {
-    setTotalPredictions(response.total_predictions);
-    console.log(response.total_predictions)
+    setTotalPredictions(response.total_predictions); // Nb total de prédictions
+    handleGraph(response.mauvaises_predictions);
 })
-// Afficher la page que quand requete effectué ! avec then ? onSetup ?
+
 
 export default function Stats_globales() {
     
+    // Déclenche la requête
     const [dataGlobale] = createResource(stats_globales_request)
-    const handleGraph = () => {
-        const ctx = document.getElementById('myChart') as HTMLCanvasElement;
-
-        // Création du graph
-        new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['Bonnes prédictions', 'Mauvaises prédictions'],
-            datasets: [{
-                label: 'Prédictions',
-                data: [90,10],
-                backgroundColor: [
-                    'rgb(54, 162, 235)',
-                    'rgb(213,0,5)'],
-                borderWidth: 0
-            }]
-        },
-        options: {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: "white"
-                    }
-                }
-            }
-        }});
-    }
-
-    //Execute la fonction après return
-    onMount(()=> {
-        handleGraph();
-    })
     
     return (
         <main class="p-4">
