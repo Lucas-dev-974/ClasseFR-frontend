15bfb28b3fcc14decbaea9ef00f0cd7835787dd7
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-03-07 14:39:46 +0400

    ajout du token d'authentification

--- src/services/services.tsx
+++ src/services/services.tsx
@@ -4,7 +4,9 @@ export const Formater = (data: any) => {
     return formdata
 }
 
+var token = localStorage.getItem("Authorization");
+
 export const request = async (url: string, method: string, data: any) => {
     const host = 'http://localhost:8000/'
-    return fetch(host + url, { method: method, body: data })
+    return fetch(host + url, { method: method, body: data, headers: {Authorization: String(token)}})
 }
